780c7bd0c22fbace318d3aee90fe2e06f2200f65
Jesse Cusack <jmcusack@ucsd.edu>
2020-08-10 15:18:02 -0700

    initial commit

---
+++ clean_all_out.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# A bash script to clean MITgcm output on comet
+
+DIR=$(readlink -f ${1})
+
+read -p "Are you sure you want to delete the contents of $(basename $DIR)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Check if it seems like an MITgcm run directory on scratch...
+if test -f "$DIR/mitgcmuv"; then
+    echo "$DIR/mitgcmuv exists so it seems safe to clean."
+else
+    echo "Could not find mitgcmuv, exiting without doing anything."
+    exit 1
+fi
+
+rm -rfv $DIR/*.data $DIR/*.meta $DIR/mnc_* $DIR/available_diagnostics.log $DIR/STD* *.nc
---
+++ copy_model_to_scratch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# A bash script to copy MITgcm models to scratch for running on comet
+
+MODEL_PATH=$(readlink -f ${1})
+echo "Copying: $MODEL_PATH"
+
+# Check the model path looks like an MITgcm dir
+if [ -d "$MODEL_PATH/input" ]; then
+  echo "Found: $MODEL_PATH/input"
+  INPUT=$MODEL_PATH/input
+else
+  echo "Could not find input directory, exiting."
+  exit 1
+fi
+
+if [ -d "$MODEL_PATH/build" ]; then
+  echo "Found: $MODEL_PATH/build"
+  BUILD=$MODEL_PATH/build
+else
+  echo "Could not find build directory, exiting."
+  exit 1
+fi
+
+SCRATCH_PATH="/oasis/scratch/comet/$USER/temp_project"
+echo "Scratch path: $SCRATCH_PATH"
+
+RUN_BASENAME=${2:-$(basename $MODEL_PATH)}
+echo "Creating: $SCRATCH_PATH/$RUN_BASENAME"
+RUN_PATH=$SCRATCH_PATH/$RUN_BASENAME
+mkdir -p $RUN_PATH
+
+echo "Copying input"
+cp -v $INPUT/*.bin $INPUT/data* $INPUT/eedata $RUN_PATH
+echo "Copying mitgcmuv"
+cp -v $BUILD/mitgcmuv $RUN_PATH
